c01baf01f3cc08af8a9adb4938029611eea8618c
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-02-11 11:53:29 +0200

    show end point

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::PostsController < Api::V1::BaseController
+    # before_action :set_restaurant, only: [ :show ]
+
   def index
     @posts = policy_scope(Post)
   end
+
+
+  def show
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  # private
+
+  # def set_restaurant
+  #   @post = Post.find(params[:id])
+  #   authorize @post  # For Pundit
+  # end
+
 end
