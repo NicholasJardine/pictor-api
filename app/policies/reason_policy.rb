5d4d0fe4a557607b8468757d728eed4b0fcd5b39
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-02-16 11:41:15 +0200

    reasons policy

---
+++ app/policies/reason_policy.rb
@@ -0,0 +1,24 @@
+class ReasonPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def create?
+    !user.nil?
+  end
+
+  def destroy?
+    update?
+  end
+
+end
