8914698b8491f37cfca24245e6ae383235e40a43
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-14 12:33:27 +0200

    active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_102807) do
+ActiveRecord::Schema.define(version: 2021_04_14_103154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,27 @@ ActiveRecord::Schema.define(version: 2021_04_14_102807) do
     t.boolean "unsatisfied", default: false
     t.boolean "not_useful", default: false
     t.index ["user_id"], name: "index_account_complaints_on_user_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "applications", force: :cascade do |t|
@@ -227,6 +248,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_102807) do
   add_foreign_key "acceptances", "private_invites"
   add_foreign_key "acceptances", "users"
   add_foreign_key "account_complaints", "users"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "applications", "user_briefs"
   add_foreign_key "applications", "users"
   add_foreign_key "articles", "users"
