fa3a2a940be018aef5562c8b7cf5710e9dc6b233
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-15 14:17:45 +0200

    more in controllers

--- app/controllers/api/v1/pb_reasons_controller.rb
+++ app/controllers/api/v1/pb_reasons_controller.rb
@@ -44,7 +44,7 @@ class Api::V1::BriefsController < Api::V1::BaseController
   end
 
   def pb_reason_params
-    params.require(:pb_reason).permit(:wants, :needs, :id, :ethnicity, :gender, :name)
+    params.require(:pb_reason).permit(:user_id, :private_brief_id, :id, :custom, :unsafe, :human_trafficking, :child_abuse, :not_legit, :sexual_content)
   end
 
   def render_error
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -44,7 +44,7 @@ class Api::V1::PostsController < Api::V1::BaseController
   end
 
   def post_params
-    params.require(:post).permit(:caption, :image, :id)
+    params.require(:post).permit(:caption, :image, :id, :video)
 
   end
 
