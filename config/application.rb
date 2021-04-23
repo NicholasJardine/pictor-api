31ee00d5831dc75adfa323480a725476498fb18b
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-23 19:57:23 +0200

    cors

--- config/application.rb
+++ config/application.rb
@@ -14,15 +14,7 @@ module PictorApi
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
-      config.load_defaults 5.1
-
-      # Settings in config/environments/* take precedence over those specified here.
-      # Application configuration should go into files in config/initializers
-      # -- all .rb files in that directory are automatically loaded.
-
-      # Only loads a smaller set of middleware suitable for API only apps.
-      # Middleware like session, flash, cookies can be added back manually.
-      # Skip views, helpers and assets when generating a new resource.
+      config.load_defaults 5.2
       config.api_only = true
 
       config.middleware.insert_before 0, "Rack::Cors" do
@@ -35,7 +27,6 @@ module PictorApi
             )
         end
       end
-    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
