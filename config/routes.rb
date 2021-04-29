fc15c3ae112bea82f686f081734ce2845488b059
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-29 13:40:30 +0200

    some changes to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-      devise_for :users
+
+  scope :api, defaults: { format: :json } do
+    devise_for :users
+  end
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [ :index, :show, :update , :create, :destroy ]
+      resources :users, only: [ :index, :show]
 
       resources :posts, only: [ :index, :show, :update , :create, :destroy ]
       resources :briefs, only: [ :index, :show, :update , :create, :destroy ]
       resources :articles, only: [ :index, :show, :update , :create, :destroy ]
       resources :user_briefs, only: [ :index, :show, :update , :create, :destroy ]
       resources :auditions, only: [ :index, :show, :update , :create, :destroy ]
-      resources :users, only: [ :index, :show, :update , :create, :destroy ]
-
     end
   end
 end
+
