fe673c206b022aba5e0b0350be7829261ff50003
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-29 14:26:14 +0200

    skip verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_perameters, if: :devise_controller?
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
 
   protected
