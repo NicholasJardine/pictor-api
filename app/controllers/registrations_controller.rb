8f397561ec48766d11120bf03f111359ea60f181
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-30 22:15:21 +0200

    registrations contoller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Users::UserRegistrationsController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
 end
 
