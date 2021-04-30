dd30df51100160894db58939efdc7b866ef97f22
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-30 16:52:13 +0200

    skip before action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
   class Api::V1::UsersController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index, :show, :create ]
     before_action :set_user, only: [ :show, :update, :destroy ]
+    skip_before_action :verify_authenticity_token
 
   def index
     @users = policy_scope(User)
