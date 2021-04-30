984a23ce3a47de92c88858a46c1eac56482ce956
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-30 11:43:36 +0200

    action dispatch flash...

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module PictorApi
     # Initialize configuration defaults for originally generated Rails version.
       config.load_defaults 5.2
       config.api_only = true
+      config.middleware.use ActionDispatch::Flash
 
       config.middleware.insert_before 0, Rack::Cors do
         allow do
