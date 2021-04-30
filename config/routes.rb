3ca38aa16ec97894c100bbe45d44d17ba8d2f168
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-04-30 17:44:13 +0200

    config.navigational

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-   devise_for :users, defaults: { format: :json }, controllers: { registrations: 'users/users' }
+   devise_for :users, defaults: { format: :json }, controllers: { registrations: 'api/v1/users/user_registrations' }
 
 
   namespace :api, defaults: { format: :json } do
