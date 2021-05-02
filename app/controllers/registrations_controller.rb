3bf3cf00738f2cd7f7611d6f5401d921c7b8b3bc
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-02 21:17:13 +0200

    registrations contoller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
 
-        def create
-        super do |resource|
-          respond_to do |format|
-            format.json { render :json => resource }
-          end
-        end
-      end
 end
 
