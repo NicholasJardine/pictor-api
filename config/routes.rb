ae92aad47e76028ceb2000a3f67c3612cfa92b7a
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-03 15:23:09 +0200

    create users is back on api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-   devise_for :users, controllers: { registrations: 'users/registrations', sessions:'users/sessions' }
+   devise_for :users
 
 
   namespace :api, defaults: { format: :json } do
