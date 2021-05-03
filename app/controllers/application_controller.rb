de0aaf25623e2e0d062c642e6655cfbd32ef2541
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-03 11:03:06 +0200

    users/user_registrations controller + routes for real

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_perameters, if: :devise_controller?
 
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
+
+
+  respond_to :json
 
   protected
 
   def configure_permitted_perameters
-    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password,:password_confirmation, :authentication_token)}
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password,:password_confirmation)}
     devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:occupation, :email, :password, :photo,:authentication_token)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:occupation, :email, :password, :password_confirmation, :current_password, :name, :age, :gender, :ethnicty, :province, :photo, :bio, :address,:contact_details, :age_range, :agent_details, :talents, :socialmedia, :website, :cv, :ciricv,:services, :cell,:tel,:company_email,:authentication_token)}
   end
