a3ed59e7978b2abf7f5b750cf480631538726773
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-03 08:05:21 +0200

    respond to json

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
 
+  respond_to :json
 end
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,6 +264,8 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = [:json]
+config.http_authenticatable_on_xhr = false
+config.navigational_formats = ["*/*", :html, :json]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
